0f65f4a5203096cce1a6d49f4770b075c0bd877f
hanbyeol <2hanbyeol1@naver.com>
2022-05-30 23:07:44 +0900

    2

--- src/components/BasicTextInput.js
+++ src/components/BasicTextInput.js
@@ -5,6 +5,7 @@ import { theme } from '../theme';
 
 const StyledTextInput = styled.TextInput`
   background-color: ${theme.textInputBackground};
+  font-size: ${props => (props.fontSize ? props.fontSize : '20px')};
   width: ${props => (props.width ? props.width : 'auto')};
   margin: ${props => (props.smargin ? props.smargin : '0')};
   padding: 5px;
@@ -13,6 +14,7 @@ const StyledTextInput = styled.TextInput`
 export default function BasicTextInput({
   placeholder,
   onChangeText,
+  fontSize,
   width,
   smargin,
   value,
@@ -21,6 +23,7 @@ export default function BasicTextInput({
     <StyledTextInput
       placeholder={placeholder}
       onChangeText={onChangeText}
+      fontSize={fontSize}
       width={width}
       smargin={smargin}
       value={value}
@@ -31,6 +34,7 @@ export default function BasicTextInput({
 BasicTextInput.propTypes = {
   placeholder: PropTypes.string,
   onChangeText: PropTypes.func.isRequired,
+  fontSize: PropTypes.string,
   width: PropTypes.string,
   smargin: PropTypes.string,
   value: PropTypes.string.isRequired,
