df88b53e9d0772328d5ed745dcc59afbe3e8e5b7
unknown <wls4370@gmail.com>
2022-06-01 19:23:58 +0900

    Impl login func

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import AuthStack from '../screens/AuthStack'
-import { ProgressContext } from '../contexts'
+import { ProgressContext, UserContext } from '../contexts'
 import Spinner from './Spinner'
+import MainStack from './MainStack'
+
 export default function Navigation() {
   const { inProgress } = useContext(ProgressContext)
+  const { user } = useContext(UserContext)
+
   return (
     <NavigationContainer>
-      <AuthStack />
+      {user?.uid && user?.email ? <MainStack /> : <AuthStack />}
       {inProgress && <Spinner />}
     </NavigationContainer>
   )
