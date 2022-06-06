58908348232b16bd49962ce6ff472113444a0b1c
unknown <wls4370@gmail.com>
2022-06-06 18:13:49 +0900

    Impl sign up using firebase

--- src/contexts/User.js
+++ src/contexts/User.js
@@ -1,14 +1,14 @@
 import React, { createContext, useState } from 'react'
 
 const UserContext = createContext({
-  user: { email: null, uid: null },
+  user: { email: null, uid: null, nickname: null },
   dispatch: () => {},
 })
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState({})
-  const dispatch = ({ email, uid }) => {
-    setUser({ email, uid })
+  const dispatch = ({ email, uid, nickname }) => {
+    setUser({ email, uid, nickname })
   }
   const value = { user, dispatch }
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
