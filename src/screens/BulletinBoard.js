f6ab9aced1eace4a03fd1ae9ff6d066030b582a5
unknown <wls4370@gmail.com>
2022-06-13 00:32:34 +0900

    Feat: retrive and search for BulleinBoard

--- src/screens/BulletinBoard.js
+++ src/screens/BulletinBoard.js
@@ -1,36 +1,58 @@
-import React, { useState } from 'react';
-import { Dimensions } from 'react-native';
-import styled from 'styled-components/native';
-import BasicButton from '../components/BasicButton';
-import BasicTextInput from '../components/BasicTextInput';
-import ArticleButton from '../components/ArticleButton';
+import React, { useContext, useEffect, useState } from 'react'
+import { Dimensions } from 'react-native'
+import styled from 'styled-components/native'
+import BasicButton from '../components/BasicButton'
+import BasicTextInput from '../components/BasicTextInput'
+import ArticleButton from '../components/ArticleButton'
+import { findAllArticles, searchBulletinArticles } from '../utils/firebase'
+import { ProgressContext } from '../contexts'
 
-const windowHeight = Dimensions.get('window').height;
+const windowHeight = Dimensions.get('window').height
 
 const MainContainer = styled.View`
-  flex: 1;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+  height: 100%;
   background-color: ${({ theme }) => theme.background};
-`;
+`
 
 const Container = styled.View`
   width: 75%;
-`;
+`
 
 const List = styled.ScrollView`
   width: 100%;
   height: ${windowHeight - 250}px;
   margin-top: 15px;
-`;
+`
 
 const ButtonView = styled.View`
+  position: absolute;
+  bottom: 25px;
   align-items: flex-end;
-`;
+`
 
 export default function BulletinBoard({ navigation }) {
+  // Context
+  const { spinner } = useContext(ProgressContext)
+
   // 필드 값
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('')
+  const [articles, setArticles] = useState([])
+  const loadArticles = async () => {
+    spinner.start()
+    setArticles(await findAllArticles({}))
+    spinner.stop()
+  }
+  useEffect(() => {
+    loadArticles()
+  }, [])
+
+  const searchArticle = async () => {
+    spinner.start()
+    setArticles(await searchBulletinArticles(search))
+    spinner.stop()
+  }
   return (
     <MainContainer>
       <Container>
@@ -39,31 +61,35 @@ export default function BulletinBoard({ navigation }) {
           onChangeText={setSearch}
           width="100%"
           value={search}
-        >
-          검색내용을 입력해주세요
-        </BasicTextInput>
-        <List>
-          <ArticleButton
-            title="제목"
-            content="내용"
-            reply={1}
-            onPress={() => {
-              navigation.navigate('BulletinBoardArticle');
-            }}
-          ></ArticleButton>
+          smargin="30px 0 0 0"
+          onSubmitEditing={searchArticle}
+        />
+
+        <List showsVerticalScrollIndicator={false}>
+          {articles.map((article, index) => (
+            <ArticleButton
+              key={index}
+              title={article.title}
+              content={article.content}
+              reply={1}
+              onPress={() => {
+                navigation.navigate('BulletinBoardArticle')
+              }}
+            />
+          ))}
         </List>
-        <ButtonView>
-          <BasicButton
-            title="게시글 작성"
-            onPress={() => {
-              console.log('게시글 작성 화면으로!!');
-            }}
-            width="100px"
-            smargin="8px 0"
-            isFilled
-          ></BasicButton>
-        </ButtonView>
       </Container>
+      <ButtonView>
+        <BasicButton
+          title="게시글 작성"
+          onPress={() => {
+            console.log('게시글 작성 화면으로!!')
+          }}
+          width="100px"
+          isFilled
+          fontSize="15px"
+        ></BasicButton>
+      </ButtonView>
     </MainContainer>
-  );
+  )
 }
