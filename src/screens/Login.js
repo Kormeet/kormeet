86676b8391a2093906de33b7c743388ac1088315
unknown <wls4370@gmail.com>
2022-06-12 16:44:46 +0900

    Impl ResetPassword

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,56 +1,79 @@
-import React, { useContext, useEffect, useState } from 'react';
-import styled from 'styled-components/native';
-import { Alert, Image } from 'react-native';
-import BasicButton from '../components/BasicButton';
-import BasicTextInput from '../components/BasicTextInput';
-import Logo from '../../assets/images/logo.png';
-import { ProgressContext, UserContext } from '../contexts';
-import { login } from '../utils/firebase';
+import React, { useContext, useEffect, useState } from 'react'
+import styled from 'styled-components/native'
+import { Alert, Image } from 'react-native'
+import BasicButton from '../components/BasicButton'
+import BasicTextInput from '../components/BasicTextInput'
+import Logo from '../../assets/images/logo.png'
+import { ProgressContext, UserContext } from '../contexts'
+import { login } from '../utils/firebase'
 
 const MainContainer = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.background};
-`;
+`
 
 const Container = styled.View`
   width: 70%;
-`;
+`
 
 const RowContainer = styled.View`
   flex-direction: row;
   justify-content: space-around;
-`;
+`
 
 export default function Login({ navigation }) {
-  const { dispatch } = useContext(UserContext);
-  const { spinner } = useContext(ProgressContext);
-  const [id, setId] = useState('');
-  const [pw, setPw] = useState('');
-  const [disabled, setDisabled] = useState(true);
+  const { dispatch } = useContext(UserContext)
+  const { spinner } = useContext(ProgressContext)
+  const [id, setId] = useState('')
+  const [pw, setPw] = useState('')
+  const [disabled, setDisabled] = useState(true)
 
   useEffect(() => {
-    setDisabled(!(id && pw));
-  }, [id, pw]);
+    setDisabled(!(id && pw))
+  }, [id, pw])
 
   const onLoginButtonPressed = async () => {
     try {
-      spinner.start();
-      const email = id.trim();
-      const password = pw.trim();
-      const user = await login({ email, password });
+      spinner.start()
+      const email = id.trim()
+      const password = pw.trim()
+      const user = await login({ email, password })
       dispatch({
         email: user.email,
         uid: user.uid,
         nickname: user.nickname,
-      });
+      })
     } catch (e) {
-      Alert.alert('Login Error', e.message);
+      const errorCode = e.code
+      switch (errorCode) {
+        case 'auth/wrong-password':
+          Alert.alert(
+            '로그인 실패',
+            '비밀번호가 일치하지 않습니다. 비밀번호는 6자 이상이어야 합니다.'
+          )
+          break
+        case 'auth/user-disabled':
+          Alert.alert('로그인 실패', '사용이 정지된 계정입니다..')
+          break
+        case 'auth/user-not-found':
+          Alert.alert(
+            '로그인 실패',
+            '해당하는 이메일의 계정을 찾을 수 없습니다.'
+          )
+          break
+        case 'auth/invalid-email':
+          Alert.alert(
+            '로그인 실패',
+            '아이디 입력이 이메일 형식에 맞지 않습니다.'
+          )
+          break
+      }
     } finally {
-      spinner.stop();
+      spinner.stop()
     }
-  };
+  }
 
   return (
     <MainContainer>
@@ -102,5 +125,5 @@ export default function Login({ navigation }) {
         </RowContainer>
       </Container>
     </MainContainer>
-  );
+  )
 }
