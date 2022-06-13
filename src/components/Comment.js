bf81e446a87505e2a01ff29b8bce31beb0f1820d
unknown <wls4370@gmail.com>
2022-06-13 18:41:29 +0900

    Feat: create&delete Reply

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,46 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components/native';
-import { theme } from '../theme';
-import BasicButton from './BasicButton';
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components/native'
+import { theme } from '../theme'
+import BasicButton from './BasicButton'
 
 const Container = styled.View`
+  width: 100%;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-`;
+`
 
-const ContentView = styled.View``;
+const ContentView = styled.View`
+  width: ${({ isMine }) => (isMine ? '80%' : '100%')};
+`
 
 const Nickname = styled.Text`
   font-size: 20px;
-  color: ${props => (props.isMine ? theme.myCommentText : theme.text)};
-`;
+  color: ${(props) => (props.isMine ? theme.myCommentText : theme.text)};
+`
 
 const Content = styled.Text`
   font-size: 20px;
   color: ${theme.text};
-`;
+`
 
-export default function Comment({ nickname, content, isMine }) {
-  return isMine ? (
+export default function Comment({
+  nickname,
+  content,
+  isMine,
+  onDeleteClicked,
+}) {
+  return (
     <Container>
-      <ContentView>
-        <Nickname isMine>{nickname}</Nickname>
+      <ContentView isMine={isMine}>
+        <Nickname isMine={isMine}>{nickname}</Nickname>
         <Content>ㄴ{content}</Content>
       </ContentView>
-      <BasicButton title="삭제" fontSize="20px" isFilled></BasicButton>
+      {isMine && (
+        <BasicButton
+          title="삭제"
+          fontSize="15px"
+          isFilled
+          onPress={onDeleteClicked}
+        />
+      )}
     </Container>
-  ) : (
-    <ContentView>
-      <Nickname>{nickname}</Nickname>
-      <Content>ㄴ{content}</Content>
-    </ContentView>
-  );
+  )
 }
 
 Content.propTypes = {
   nickname: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   isMine: PropTypes.bool,
-};
+}
