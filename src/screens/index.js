a84ee8e7bbb1c08131fa357fadade153c91574b6
LeeChoongHwan <choo901@naver.com>
2022-06-14 09:56:07 +0900

    BulletinBoardWrite implement

--- src/screens/index.js
+++ src/screens/index.js
@@ -8,6 +8,7 @@ import Channel from './Channel'
 import ChannelCreation from './ChannelCreation'
 import MainPage from './MainPage'
 import MyInfo from './MyInfo'
+import BulletinBoardWrite from './BulletinBoardWrite'
 
 export {
   Login,
@@ -20,4 +21,5 @@ export {
   ChannelCreation,
   MainPage,
   MyInfo,
+  BulletinBoardWrite,
 }
