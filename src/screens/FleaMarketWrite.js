e586b341881dc862bde08c4e44ccaf03c0c93cd2
unknown <wls4370@gmail.com>
2022-06-15 17:28:10 +0900

    Impl FleaMarket view, create, delete

--- src/screens/FleaMarketWrite.js
+++ src/screens/FleaMarketWrite.js
@@ -1,78 +1,67 @@
-import React, { useContext, useEffect, useLayoutEffect, useState } from 'react'
-import styled from 'styled-components/native'
+import React, { useEffect, useState } from 'react'
 import BasicTextInput from '../components/BasicTextInput'
-import BasicButton from '../components/BasicButton'
-import { Alert, Text } from 'react-native'
-import { createArticle } from '../utils/firebase'
-import { UserContext } from '../contexts'
+import { Alert, View } from 'react-native'
+import { createFleaMarketArticleInfo } from '../utils/firebase'
 import ArticleWrite from './ArticleWrite'
 
 export default function FleaMarketWrite({ navigation, route }) {
-  const { user } = useContext(UserContext)
-
-  const [title, setTitle] = useState('')
-  const [content, setContent] = useState('')
   const [price, setPrice] = useState('')
   const [place, setPlace] = useState('')
 
-  const { articleType } = route.params
+  useEffect(() => {
+    if (!/^[0-9]+$/.test(price)) {
+      if (price.length == 1) setPrice('')
+      else setPrice((p) => p.substring(0, p.length - 1))
+    }
+  }, [price])
 
-  const onClickSubmit = async () => {
-    const _title = title.trim()
-    const _content = content.trim()
+  const beforeSubmit = () => {
     const _price = price.trim()
-    const_place = place.trim()
-
-    if (_title.length < 1) {
-      Alert.alert('게시글 작성', '제목을 입력해주세요')
-      return
-    }
-    if (_content.length < 1) {
-      Alert.alert('게시글 작성', '내용을 입력해주세요')
-      return
-    }
+    const _place = place.trim()
     if (_price.length < 1) {
       Alert.alert('게시글 작성', '가격을 입력해주세요')
+      return false
     }
     if (_place.length < 1) {
-      Alert.alert('게시글 작성', '거래 장소를 입력해주세요')
+      Alert.alert('게시글 작성', '장소를 입력해주세요')
+      return false
     }
-    await createArticle({
-      title: _title,
-      content: _content,
-      type: articleType,
-      userId: user.id,
+    return true
+  }
+
+  const onSubmit = async (articleId) => {
+    await createFleaMarketArticleInfo({
+      place: place.trim(),
+      price: Number.parseInt(price.trim()),
+      articleId,
     })
-    // switch (articleType) {
-    //   case 'BULLETIN':
-    //     navigation.navigate('BulletinBoard')
-    //     return
-    //   case 'FLEA_MARKET':
-    //     navigation.navigate('FleaMarket')
-    //     return
-    //   case 'JOB_SEARCH':
-    //     navigation.navigate('JobSearch')
-    //     return
-    // }
   }
   return (
-    <ArticleWrite navigation={navigation} route={route}>
-      <BasicTextInput
-        onChangeText={setPrice}
-        placeholder="가격"
-        fontSize="20px"
-        width="100%"
-        smargin="0 0 5px 0"
-        value={price}
-      />
-      <BasicTextInput
-        onChangeText={setPlace}
-        placeholder="위치"
-        fontSize="20px"
-        width="100%"
-        smargin="0 0 5px 0"
-        value={place}
-      />
-    </ArticleWrite>
+    <ArticleWrite
+      navigation={navigation}
+      route={route}
+      beforeSubmit={beforeSubmit}
+      onSubmit={onSubmit}
+      Inputs={
+        <View>
+          <BasicTextInput
+            onChangeText={setPrice}
+            placeholder="가격"
+            fontSize="20px"
+            width="100%"
+            smargin="0 0 5px 0"
+            value={price}
+          />
+          <BasicTextInput
+            onChangeText={setPlace}
+            placeholder="위치"
+            fontSize="20px"
+            width="100%"
+            smargin="0 0 10px 0"
+            value={place}
+          />
+        </View>
+      }
+    />
   )
 }
